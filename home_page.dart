458094e77fb5645d6028f9b200b1b0e59896376f
duaaanis <103386530+duaaanis@users.noreply.github.com>
2022-04-18 21:58:22 +0500

    Add files via upload

--- home_page.dart
+++ home_page.dart
@@ -1,1 +1,55 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter_application_1/models/catalog.dart';
+import 'package:flutter_application_1/widget/drawer.dart';
+import 'package:flutter_application_1/widget/itaem_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "CodePur";
+
+  @override
+  void initState() {
+    super.initState();
+    lodeData();
+  }
+
+  loadData() async {
+    final catalogjson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogjson);
+    var productsData = decodeData["products"];
+    print(decodeData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModels.Item[0]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+
+  void lodeData() {}
+}
